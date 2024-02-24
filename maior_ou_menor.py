1100d930ecda09cef9c09cf827305117b143aa86
wesley souza <122413661+Wesleyszs@users.noreply.github.com>
2024-02-23 21:10:10 -0300

    Create maior_ou_menor.py
    codigo simples , de maior ou menor nos números digitados

---
+++ maior_ou_menor.py
@@ -0,0 +1,7 @@
+primeiro_valor=input('digite um valor: ')
+segundo_valor=input('digite um segundo valor ')
+
+if primeiro_valor > segundo_valor:
+    print('o primeiro valor é maior')
+elif segundo_valor > primeiro_valor:
+    print('o segundo valor é maior ') 
